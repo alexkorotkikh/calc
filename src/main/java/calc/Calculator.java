d357079a0f87aa2cc570251a6c6dd395fd0e9525
Alex Korotkikh <a.korotkikh@gmail.com>
2013-06-22 11:43:04 +0300

    initial commit

---
+++ src/main/java/calc/Calculator.java
@@ -0,0 +1,7 @@
+package calc;
+
+public class Calculator {
+    public int calculate(final String str) {
+        return 0;
+    }
+}
---
+++ src/test/java/calc/CalculatorTest.java
@@ -0,0 +1,50 @@
+package calc;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * User: alexkorotkikh
+ * Date: 6/22/13
+ * Time: 10:20 AM
+ */
+@RunWith(Parameterized.class)
+public class CalculatorTest {
+    private final String strInput;
+    private final int result;
+
+    public CalculatorTest(final String strInput, final int result) {
+        this.strInput = strInput;
+        this.result = result;
+    }
+
+    private Calculator calc;
+
+    @Before
+    public void setUp() throws Exception {
+        calc = new Calculator();
+    }
+
+    @Test
+    public void testCalculate() throws Exception {
+        assertEquals(result, calc.calculate(strInput));
+    }
+
+    @Parameterized.Parameters
+    public static Collection data() {
+        return Arrays.asList(new Object[][]{
+                {"1+1", 2},
+                {"2 + 3", 5},
+                {"10+ 1", 11},
+                {"2 -5", -3},
+                {"10 - 11", -1}
+        });
+    }
+}
